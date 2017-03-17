106ea1a8b99cad0c45aa207f209b2c4f35386c49
tamerhn <t.nana@kindiplus.com>
2017-03-18 01:53:22 +0200

    Add: step-5 is done

--- main.fsx
+++ main.fsx
@@ -1,6 +1,9 @@
 open System
+open System.Collections.Generic
+
+exception NegativityError of string
 //the entered string
-let orginalStr =  "//;\n9;1;8"
+let orginalStr =  "//;\n9;-11;-8"
 //split the given string to the data an the delimerter
 let splitString (str:string) =
     str.Split('\n')
@@ -27,15 +30,27 @@ let sliceArr (start:int) (length:int) (arr:string[]) =
         Array.sub arr start length
     else
         arr
-
+//check for negative numbers
+let checkNegativity (arr:int []) =
+    let negatives = arr |> Array.filter (fun x -> x < 0)
+    if (negatives.Length > 0) then
+        let error = negatives |> Array.map string |> String.concat ", "
+        raise(NegativityError("Negatives not allowed " + error))
+    arr
 //the add function
 let add orginalStr =
     let str = orginalStr |> splitString |> getString
     let splitBy = [|orginalStr |> splitString |> getSplitter|]
-    str
-    |> split splitBy 
-    |> Array.map int
-    |> Array.sum
+    let mutable sum = 1
+    try
+        sum <- str
+                |> split splitBy 
+                |> Array.map int
+                |> checkNegativity
+                |> Array.sum
+    with 
+    | NegativityError err -> failwith err
+    sum
 
 
 printfn "Sum is: %d" (add orginalStr)
