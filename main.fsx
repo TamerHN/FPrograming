e7a963dd0796a7f8f5cbed37761001147f31eb6f
tamerhn <t.nana@kindiplus.com>
2017-03-18 01:27:35 +0200

    Add: step-4 is done

--- main.fsx
+++ main.fsx
@@ -1,6 +1,23 @@
 open System
-let str = "1,5\n6,8,9" 
-let splitBy = [|","; "\n"|]
+//the entered string
+let orginalStr =  "//;\n9;1;8"
+//split the given string to the data an the delimerter
+let splitString (str:string) =
+    str.Split('\n')
+//get the splitter, or ',' by default
+let getSplitter (str:string []) = 
+    if str.Length <> 1 then
+        str.[0].Split([|"//"|], StringSplitOptions.RemoveEmptyEntries).[0]
+    else 
+        ","
+//get the data (numbers)
+let getString (str:string []) =
+    if str.Length <> 1 then 
+        str.[1]
+    else 
+        str.[0]
+
+//get the numbers from the data string
 let split (splitBy:string[]) (str:string)  = 
     str.Split(splitBy, StringSplitOptions.RemoveEmptyEntries)
 
@@ -12,11 +29,13 @@ let sliceArr (start:int) (length:int) (arr:string[]) =
         arr
 
 //the add function
-let add str =
+let add orginalStr =
+    let str = orginalStr |> splitString |> getString
+    let splitBy = [|orginalStr |> splitString |> getSplitter|]
     str
     |> split splitBy 
     |> Array.map int
     |> Array.sum
 
 
-printfn "Sum is: %d" (add str)
+printfn "Sum is: %d" (add orginalStr)
